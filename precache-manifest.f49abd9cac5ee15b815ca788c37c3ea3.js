a8bcc6fbcdaf7df3e3dc2c469445df0176fbf66f
vinayden1 <vinayden1@gmail.com>
2020-10-24 20:14:19 -0600

    Updates

---
+++ precache-manifest.f49abd9cac5ee15b815ca788c37c3ea3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b0e7fe1936e1b4dbea902c47acc1fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e939b5acfdac840e1038",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4991c8fbaab3535d8a2f",
+    "url": "/static/css/main.42cee457.chunk.css"
+  },
+  {
+    "revision": "e939b5acfdac840e1038",
+    "url": "/static/js/2.73f59e0f.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/static/js/2.73f59e0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4991c8fbaab3535d8a2f",
+    "url": "/static/js/main.1ffa6822.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
